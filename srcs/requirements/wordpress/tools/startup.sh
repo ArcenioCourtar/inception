27b05f982951ca724b72e19343dfd5d4ed6db6df
Arcenio Courtar <71286201+ArcenioCourtar@users.noreply.github.com>
2026-07-26 20:52:04 +0200

    3 containers, all running ports exposed.
    How do I make them talk to each other though?

--- srcs/requirements/wordpress/tools/startup.sh
+++ srcs/requirements/wordpress/tools/startup.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
-# exit if any of my commands return an error
 set -e
 
-# DO STUFF!
-echo "HELLO"
-echo "GOODBYE"
+mkdir -p /var/www/html/wordpress
+touch /run/php/php8.2-fpm.pid;
+chown -R www-data:www-data /var/www/*;
+chown -R 755 /var/www/*;
+
+# Download/Install WordPress if not already present
+if [ ! -f wp-config.php ]; then
+
+    	# Install WP-CLI (wp command)
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
+    
+    	cd /var/www/html/wordpress
+    
+    	# Downloading WordPress"
+   	wp core download \
+        	--path="/var/www/html/wordpress/" \
+        	--allow-root
+    	echo "WordPress downloaded."
+else
+	echo "WordPress is already downloaded"
+fi
+
+echo "Starting PHP-FPM"
+exec /usr/sbin/php-fpm8.2 -F
